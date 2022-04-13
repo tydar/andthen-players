8f29504bd96d89f03f1100f07c8cf2d6387468f4
Tyler Darnell <tyler.a.darnell@gmail.com>
2022-04-13 16:36:46 -0500

    Implement /players/token route
    Route sets a cookie with the player id stored in a JWT. This will be called by the frontend after they received their
    Bearer token from the auth service. Then this player id cookie can be read and verified by the games service.

--- routes/players.js
+++ routes/players.js
@@ -4,26 +4,10 @@ import * as jose from 'jose';
 
 export const router = new Router();
 
-router.get('/:id', async (req, res) => {
-	const { id } = req.params;
-
-	try {
-		const { rows } = await db.query('SELECT * FROM players WHERE id = $1', [id]);
-
-		if (Array.isArray(rows) && rows.length > 0) {
-			res.send({ status: 'success', data: rows[0] });
-			return;
-		}
-		res.send({status: 'fail', data: {'message': `player record not found with id ${id}`}});
-	} catch (err) {
-		console.log(err.stack);
-		res.send({
-			status: 'error', 
-			message: 'Database error', 
-			code: 500
-		});
-	}
-});
+const stringToKey = (str) => {
+	const encoder = new TextEncoder();
+	return encoder.encode(str);
+};
 
 router.get('/user/:id', async (req, res) => {
 	const { id } = req.params;
@@ -32,15 +16,82 @@ router.get('/user/:id', async (req, res) => {
 		const { rows } = await db.query('SELECT * FROM players WHERE user_id = $1', [id]);
 
 		if (Array.isArray(rows) && rows.length > 0) {
-			res.send({ status: 'success', data: rows[0] });
+			res.json({ status: 'success', data: rows[0] });
 			return;
 		}
-		res.send({status: 'fail', data:{'message': `player record not found with user id ${id}`}});
+		res.json({status: 'fail', data:{'message': `player record not found with user id ${id}`}});
 	} catch (err) {
 		console.log(err.stack);
-		res.send({
+		res.json({
 			status: 'error',
 			message: 'Database error',
+			code: 500
+		});
+	}
+});
+
+router.get('/token', async (req, res) => {
+	// if the user is authenticated
+	// give them the token for their player id
+	// or give an error
+	const authHeader = req.get('Authorization');
+	if (typeof authHeader === 'undefined') {
+		res.json({status: 'error', message: 'not authenticated', code: 401, data:{'WWW-Authenticate': 'Bearer'}});
+	}
+
+	if (!authHeader.startsWith('Bearer ')) {
+		res.json({status: 'error', message: 'expected Bearer token', code: 400});
+	}
+
+	const jwt = authHeader.slice("Bearer ".length);
+	const { payload } = await jose.jwtVerify(jwt, stringToKey("your-256-bit-secret"));
+	const { id } = payload;
+
+	if (typeof id !== 'number') {
+		res.json({status: 'error', message: `bad token payload type: ${typeof id}`});
+	}
+
+	var dbRes;
+	try {
+		dbRes = await db.query(
+			'SELECT id FROM players WHERE user_id = $1',
+			[id]
+		);
+
+		if (!Array.isArray(dbRes.rows) || dbRes.rows.length === 0) {
+			console.log(dbRes);
+			res.json({status: 'fail', data: {message: 'missing user record', db_response: dbRes.rows}});
+		}
+	} catch(err) {
+		res.json({status: 'error', message: 'internal error', code: 500});
+	}
+
+	const playerJwt = await new jose.SignJWT({ 'player_id': dbRes.rows[0].id })
+		.setProtectedHeader({alg: 'HS256'})
+		.setIssuedAt()
+		.setExpirationTime('1h')
+		.sign(stringToKey('your-256-bit-secret'));
+
+	res.cookie('player_jwt', playerJwt, {httpOnly: true, expires: new Date(Date.now() + 3600000)});
+	res.json({status: 'success', data: null});
+});
+
+router.get('/get/:id', async (req, res) => {
+	const { id } = req.params;
+
+	try {
+		const { rows } = await db.query('SELECT * FROM players WHERE id = $1', [id]);
+
+		if (Array.isArray(rows) && rows.length > 0) {
+			res.json({ status: 'success', data: rows[0] });
+			return;
+		}
+		res.json({status: 'fail', data: {'message': `player record not found with id ${id}`}});
+	} catch (err) {
+		console.log(err.stack);
+		res.json({
+			status: 'error', 
+			message: 'Database error', 
 			code: 500
 		});
 	}
@@ -49,32 +100,32 @@ router.get('/user/:id', async (req, res) => {
 router.post('/', async (req, res) => {
 	const { authHeader } = req.get('Authorization');
 	if (typeof authHeader === 'undefined') {
-		res.send({status: 'error', message: 'not authenticated', code: 401, data:{'WWW-Authenticate': 'Bearer'}});
+		res.json({status: 'error', message: 'not authenticated', code: 401, data:{'WWW-Authenticate': 'Bearer'}});
 		return;
 	}
 
 	if (!authHeader.startsWith('Bearer ')) {
-		res.send({status: 'error', message: 'expected Bearer token', code: 400});
+		res.json({status: 'error', message: 'expected Bearer token', code: 400});
 		return;
 	}
 
 	const jwt = authHeader.slice("Bearer ".length);
-	const { payload } = await jose.jwtVerify(jwt, "your-256-bit-secret");
+	const { payload } = await jose.jwtVerify(jwt, stringToKey("your-256-bit-secret"));
 	const { admin } = payload;
 
 	if (typeof admin !== 'boolean') {
-		res.send({status: 'error', message: 'not authorized', code: 403});
+		res.json({status: 'error', message: 'not authorized', code: 403});
 		return;
 	}
 
 	if (!admin) {
-		res.send({status: 'error', message: 'not authorized', code: 403});
+		res.json({status: 'error', message: 'not authorized', code: 403});
 		return;
 	}
 
 	const { display_name = "", user_id } = req.body;
 	if (typeof user_id === 'undefined') {
-		res.send({status: 'error', message: 'user_id required in request object', code: 400});
+		res.json({status: 'error', message: 'user_id required in request object', code: 400});
 		return;
 	}
 
@@ -85,13 +136,13 @@ router.post('/', async (req, res) => {
 		);
 
 		if (Array.isArray(rows) && rows.length > 0) {
-			res.send({ status: 'success', data: rows[0] });
+			res.json({ status: 'success', data: rows[0] });
 			return;
 		} 
-		res.send({status: 'fail', data:{message: 'new player failed to create'}});
+		res.json({status: 'fail', data:{message: 'new player failed to create'}});
 	} catch(err) {
 		console.log(err.stack);
-		res.send({
+		res.json({
 			status: 'error',
 			message: 'Database error',
 			code: 500
